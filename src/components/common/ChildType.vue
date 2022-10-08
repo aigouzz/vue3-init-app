273fba17bc0eda715239abf9ddd49e1622dc49f7
gxc <1003823149@qq.com>
2022-10-08 15:50:57 +0800

    add project

---
+++ src/components/common/ChildType.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="child">
+        <slot name="title"></slot>
+        <slot name="content"></slot>
+    </div>
+</template>
+<script lang="ts">
+    export default{
+        setup() {
+
+        }
+    }
+</script>
+<style lang="less" scoped>
+    .child{
+        width: 100%;
+    }
+</style>
